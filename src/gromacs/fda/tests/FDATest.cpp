629a7dc94bc04ef647690f1e38b5d433d5e1e823
Bernd Doser <bernd.doser@h-its.org>
2017-09-15 11:21:19 +0200

    Fix redmine #1109: not-a-number values in punctual stress

---
+++ src/gromacs/fda/tests/FDATest.cpp
@@ -0,0 +1,49 @@
+/*
+ * FDA.cpp
+ *
+ *  Created on: Sep 15, 2017
+ *      Author: Bernd Doser, HITS gGmbH <bernd.doser@h-its.org>
+ */
+
+#include <iostream>
+#include <gtest/gtest.h>
+#include "gromacs/fda/FDA.h"
+#include "testutils/integrationtests.h"
+
+namespace fda {
+
+//! Test fixture for PairwiseForces
+class FDATest : public ::gmx::test::IntegrationTestFixture
+{};
+
+TEST_F(FDATest, add_angle)
+{
+    FDASettings fda_settings;
+    fda_settings.atom_based_result_type = ResultType::NO;
+    fda_settings.residue_based_result_type = ResultType::PUNCTUAL_STRESS;
+    fda_settings.one_pair = OnePair::SUMMED;
+    fda_settings.v2s = Vector2Scalar::NORM;
+    fda_settings.residues_renumber = ResiduesRenumber::AUTO;
+    fda_settings.no_end_zeros = false;
+    fda_settings.syslen_atoms = 0;
+    fda_settings.syslen_residues = 0;
+    fda_settings.time_averaging_period = 1;
+    fda_settings.type = InteractionType_NONE;
+    fda_settings.nonbonded_exclusion_on = true;
+    fda_settings.bonded_exclusion_on = true;
+    fda_settings.index_group1 = -1;
+    fda_settings.index_group2 = -1;
+    fda_settings.groups = nullptr;
+    fda_settings.groupnames = nullptr;
+
+    FDA fda(fda_settings);
+    rvec f_i{0.0, 0.0, 0.0};
+    rvec f_j{0.0, 0.0, 0.0};
+    rvec f_k{0.0, 0.0, 0.0};
+    fda.add_angle(0, 1, 2, f_i, f_j, f_k);
+
+}
+
+
+
+} // namespace fda
